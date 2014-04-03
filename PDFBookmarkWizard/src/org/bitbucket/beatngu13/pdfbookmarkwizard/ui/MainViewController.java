e12ce1f6898011f8958fd7b38db3ec4abc3cc73d
danielkraus1986@gmail.com <danielkraus1986@gmail.com@192.168.1.4>
2014-04-03 23:54:03 +0200

    Switched to JDK 8. GNU GPLv3 added.

--- PDFBookmarkWizard/src/org/bitbucket/beatngu13/pdfbookmarkwizard/ui/MainViewController.java
+++ PDFBookmarkWizard/src/org/bitbucket/beatngu13/pdfbookmarkwizard/ui/MainViewController.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the PDF Bookmark Wizard.
+ * 
+ * The PDF Bookmark Wizard is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation. <br><br>
+ * 
+ * The PDF Bookmark Wizard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+ * Public License for more details. <br><br>
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * the PDF Bookmark Wizard. If not, see 
+ * <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>. <br><br>
+ * 
+ * Copyright 2013-2014 Daniel Kraus
+ */
 package org.bitbucket.beatngu13.pdfbookmarkwizard.ui;
 
 import java.io.File;
