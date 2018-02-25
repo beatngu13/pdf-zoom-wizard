a673a45ce4cb59f0037eb2e99a9f7801663fb8b9
Daniel Kraus <daniel.kraus@mailbox.org>
2018-02-25 15:14:40 +0100

    Rename lib to deps
    Better reflects what install-deps.sh is doing.

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-mvn install:install-file -Dfile=lib/org/pdfclown/pdfclown/0.1.2/pdfclown.jar -DgroupId=org.pdfclown -DartifactId=pdfclown -Dversion=0.1.2 -Dpackaging=jar
+mvn install:install-file -Dfile=deps/org/pdfclown/pdfclown/0.1.2/pdfclown.jar -DgroupId=org.pdfclown -DartifactId=pdfclown -Dversion=0.1.2 -Dpackaging=jar
