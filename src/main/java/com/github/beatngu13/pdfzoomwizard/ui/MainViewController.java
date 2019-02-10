d27f3df968ad4eca665096f4ce356a4836c84b7a
Daniel Kraus <daniel.kraus@mailbox.org>
2019-02-10 09:25:25 +0100

    Simplify lambdas

--- src/main/java/com/github/beatngu13/pdfzoomwizard/ui/MainViewController.java
+++ src/main/java/com/github/beatngu13/pdfzoomwizard/ui/MainViewController.java
@@ -161,7 +161,7 @@ public class MainViewController {
 				alert.setContentText(getConfirmationMessage());
 				alert.showAndWait() //
 						.filter(response -> response == ButtonType.OK) //
-						.ifPresent(response -> MainViewController.this.run());
+						.ifPresent(response -> run());
 			}
 		});
 	}
@@ -229,8 +229,7 @@ public class MainViewController {
 		String filenameInfix = copyCheckBox.isSelected() ? copyTextField.getText() : null;
 		Wizard wizard = new Wizard(root, filenameInfix, zoomChoiceBox.getValue());
 		Thread thread = new Thread(wizard);
-		wizard.messageProperty()
-				.addListener((observable, oldValue, newValue) -> MainViewController.this.stateText.setText(newValue));
+		wizard.messageProperty().addListener((observable, oldValue, newValue) -> stateText.setText(newValue));
 		thread.setDaemon(true);
 		thread.start();
 	}
