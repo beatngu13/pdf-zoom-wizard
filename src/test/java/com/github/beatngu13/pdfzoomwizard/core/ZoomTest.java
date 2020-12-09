78ea74b1f914ecc9c153b1b763b8dda75227e59e
Daniel Kraus <daniel.kraus@mailbox.org>
2020-12-09 23:02:25 +0100

    Convert to parameterized test

--- src/test/java/com/github/beatngu13/pdfzoomwizard/core/ZoomTest.java
+++ src/test/java/com/github/beatngu13/pdfzoomwizard/core/ZoomTest.java
@@ -1,18 +1,30 @@
 package com.github.beatngu13.pdfzoomwizard.core;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ZoomTest {
 
-	@Test
-	void to_string_should_be_well_formated() {
-		assertThat(Zoom.ACTUAL_SIZE).hasToString("Actual size");
-		assertThat(Zoom.FIT_PAGE).hasToString("Fit page");
-		assertThat(Zoom.FIT_VISIBLE).hasToString("Fit visible");
-		assertThat(Zoom.FIT_WIDTH).hasToString("Fit width");
-		assertThat(Zoom.INHERIT_ZOOM).hasToString("Inherit zoom");
+	@ParameterizedTest
+	@MethodSource("args")
+	void to_string_should_be_formatted(Zoom zoom, String formatted) {
+		assertThat(zoom).hasToString(formatted);
+	}
+
+	static Stream<Arguments> args() {
+		return Stream.of(
+				arguments(Zoom.ACTUAL_SIZE, "Actual size"),
+				arguments(Zoom.FIT_PAGE, "Fit page"),
+				arguments(Zoom.FIT_VISIBLE, "Fit visible"),
+				arguments(Zoom.FIT_WIDTH, "Fit width"),
+				arguments(Zoom.INHERIT_ZOOM, "Inherit zoom")
+		);
 	}
 
 }
