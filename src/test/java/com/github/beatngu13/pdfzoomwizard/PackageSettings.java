1677bb55f2c019319aa0de843b798c6e904b4b94
Daniel Kraus <daniel.kraus@mailbox.org>
2021-01-17 00:34:02 +0100

    Move ApprovalTests GMs to resources

---
+++ src/test/java/com/github/beatngu13/pdfzoomwizard/PackageSettings.java
@@ -0,0 +1,10 @@
+package com.github.beatngu13.pdfzoomwizard;
+
+/**
+ * ApprovalTests configuration.
+ */
+public class PackageSettings {
+
+	private static final String ApprovalBaseDirectory = "../resources/";
+
+}
