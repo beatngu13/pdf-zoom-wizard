a5b0576dea1bdab528f007f9be3b4b9933d45c12
Daniel Kraus <daniel.kraus@mailbox.org>
2021-03-13 17:31:11 +0100

    Add Gluon Client Maven plugin

---
+++ src/main/java/com/github/beatngu13/pdfzoomwizard/LauncherWrapper.java
@@ -0,0 +1,12 @@
+package com.github.beatngu13.pdfzoomwizard;
+
+/**
+ * See https://stackoverflow.com/a/52654791.
+ */
+public class LauncherWrapper {
+
+	public static void main(String[] args) {
+		Launcher.main(args);
+	}
+
+}
