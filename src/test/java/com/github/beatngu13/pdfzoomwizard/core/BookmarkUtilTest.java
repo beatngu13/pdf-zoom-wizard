a7afaacd2a44d62d9936c032c6bea843ff319e50
Daniel Kraus <daniel.kraus@mailbox.org>
2023-10-25 20:04:56 +0200

    Use MockitoExtension

--- src/test/java/com/github/beatngu13/pdfzoomwizard/core/BookmarkUtilTest.java
+++ src/test/java/com/github/beatngu13/pdfzoomwizard/core/BookmarkUtilTest.java
@@ -1,25 +1,29 @@
 package com.github.beatngu13.pdfzoomwizard.core;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.pdfclown.documents.interaction.navigation.document.Bookmark;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BookmarkUtilTest {
+
+	@Mock
+	Bookmark bookmark;
 
 	@Test
 	void bookmark_title_should_be_used_when_available() {
 		var title = "foo";
-		var bookmark = mock(Bookmark.class);
 		when(bookmark.getTitle()).thenReturn(title);
 		assertThat(BookmarkUtil.getTitle(bookmark)).isEqualTo(title);
 	}
 
 	@Test
 	void fallback_title_should_be_used_when_exception_occurs() {
-		var bookmark = mock(Bookmark.class);
 		when(bookmark.getTitle()).thenThrow(RuntimeException.class);
 		assertThat(BookmarkUtil.getTitle(bookmark)).isEqualTo(BookmarkUtil.BOOKMARK_TITLE_FALLBACK);
 	}
--- src/test/java/com/github/beatngu13/pdfzoomwizard/core/WizardTest.java
+++ src/test/java/com/github/beatngu13/pdfzoomwizard/core/WizardTest.java
@@ -1,50 +1,60 @@
 package com.github.beatngu13.pdfzoomwizard.core;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mock.Strictness;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.pdfclown.documents.interaction.navigation.document.Bookmark;
 import org.pdfclown.documents.interaction.navigation.document.Bookmarks;
-import org.pdfclown.objects.PdfObjectWrapper;
 
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class WizardTest {
+
+	@Mock
+	Bookmark bookmark;
+	@Mock
+	Bookmarks bookmarks;
+	@Mock(strictness = Strictness.LENIENT)
+	Bookmarks childBookmarks;
+
+	Iterator<Bookmark> bookmarksIterator;
+	Iterator<Bookmark> childBookmarksIterator;
+
+	Wizard cut = new Wizard(null, null, Zoom.ACTUAL_SIZE);
+
+	@BeforeEach
+	void setUp() {
+		bookmarksIterator = List.of(bookmark).iterator();
+		childBookmarksIterator = Collections.emptyIterator();
+
+		when(bookmark.getBookmarks()).thenReturn(childBookmarks);
+		when(bookmarks.iterator()).thenReturn(bookmarksIterator);
+		when(childBookmarks.iterator()).thenReturn(childBookmarksIterator);
+	}
 
 	@Test
 	void get_target_exception_should_not_crash_execution() {
-		var cut = new Wizard(null, null, Zoom.ACTUAL_SIZE);
-
-		var bookmark = mock(Bookmark.class);
-		when(bookmark.getBookmarks()).thenReturn(mock(Bookmarks.class));
 		when(bookmark.getTarget()).thenThrow(RuntimeException.class);
-
-		var bookmarkIter = Collections.singleton(bookmark).iterator();
-
-		var bookmarks = mock(Bookmarks.class);
-		when(bookmarks.iterator()).thenReturn(bookmarkIter);
 
 		assertThatCode(() -> cut.modifyBookmarks(bookmarks)).doesNotThrowAnyException();
 	}
 
 	@Test
 	void get_target_null_should_not_crash_execution() {
-		var cut = new Wizard(null, null, Zoom.ACTUAL_SIZE);
-
-		var bookmark = mock(Bookmark.class);
-		when(bookmark.getBookmarks()).thenReturn(mock(Bookmarks.class));
 		when(bookmark.getTarget()).thenReturn(null);
-
-		var bookmarkIter = Collections.singleton(bookmark).iterator();
-
-		var bookmarks = mock(Bookmarks.class);
-		when(bookmarks.iterator()).thenReturn(bookmarkIter);
 
 		assertThatCode(() -> cut.modifyBookmarks(bookmarks)).doesNotThrowAnyException();
 	}
@@ -52,23 +62,8 @@ class WizardTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	void closed_bookmarks_should_be_modified() {
-		var cut = spy(new Wizard(null, null, Zoom.ACTUAL_SIZE));
-
-		var childBookmarksIter = Collections.<Bookmark>emptyIterator();
-
-		var childBookmarks = mock(Bookmarks.class);
-		when(childBookmarks.iterator()).thenReturn(childBookmarksIter);
-		// Negative count means closed bookmark.
+		var cut = spy(this.cut);
 		when(childBookmarks.size()).thenReturn(-1);
-
-		var bookmark = mock(Bookmark.class);
-		when(bookmark.getBookmarks()).thenReturn(childBookmarks);
-		when(bookmark.getTarget()).thenReturn(mock(PdfObjectWrapper.class));
-
-		var bookmarksIter = Collections.singleton(bookmark).iterator();
-
-		var bookmarks = mock(Bookmarks.class);
-		when(bookmarks.iterator()).thenReturn(bookmarksIter);
 
 		cut.modifyBookmarks(bookmarks);
 
@@ -78,17 +73,8 @@ class WizardTest {
 	@Test
 	@Timeout(1)
 	void endless_bookmark_iterator_should_be_stopped() {
-		var cut = new Wizard(null, null, Zoom.ACTUAL_SIZE);
-
-		var childBookmarks = mock(Bookmarks.class);
-
-		var bookmark = mock(Bookmark.class);
-		when(bookmark.getBookmarks()).thenReturn(childBookmarks);
-
-		var bookmarksIter = Stream.generate(() -> bookmark).iterator();
-
-		var bookmarks = mock(Bookmarks.class);
-		when(bookmarks.iterator()).thenReturn(bookmarksIter);
+		var bookmarksIterator = Stream.generate(() -> bookmark).iterator();
+		when(bookmarks.iterator()).thenReturn(bookmarksIterator);
 
 		assertThatCode(() -> cut.modifyBookmarks(bookmarks)).doesNotThrowAnyException();
 	}
